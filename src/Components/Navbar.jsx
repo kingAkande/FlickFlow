90b2c593bf4d5cd4ce15c518ee24dc50a740e414
KingAkande <kingakande@gmail.com>
2025-02-13 00:35:24 +0100

    Navbar component created and opdated

--- src/Components/Navbar.jsx
+++ src/Components/Navbar.jsx
@@ -1,27 +1,48 @@
 /* eslint-disable react/prop-types */
 /* eslint-disable react/no-unknown-property */
 /* eslint-disable no-unused-vars */
-import React from 'react'
+import React from "react";
 
-const Navbar = ({setQuery}) => {
+const Navbar = ({ setQuery, query, movies }) => {
   return (
-    <nav className="">
-    <div className="logo">
-      <span role="img">🍿</span>
-      <h1>usePopcorn</h1>
-    </div>
-    <input
-      className="search"
-      type="text"
-      placeholder="Search movies..."telecom APIs and documentation
-      value={query}
-      onChange={(e) => setQuery(e.target.value)}
-    />
-    <p className="num-results">
-      Found <strong>{movies.length}</strong> results
-    </p>
-  </nav>
-  )
-}
+    // <nav className="grid ">
+    //   <div className="">
+    //     <span role="img">🍿</span>
+    //     <h1>usePopcorn</h1>
+    //   </div>
+    //   <input
+    //     className="search"
+    //     type="text"
+    //     placeholder="Search movies..."
+    //     telecom
+    //     APIs
+    //     and
+    //     documentation
+    //     value={query}
+    //     onChange={(e) => setQuery(e.target.value)}
+    //   />
+    //   <p className="num-results">
+    //     Found <strong>{movies.length}</strong> results
+    //   </p>
+    // </nav>
 
-export default Navbar
+    <nav className="grid grid-cols-3 items-center h-28 px-8 bg-[#6741d9] rounded-lg">
+      <div className="flex items-center gap-2">
+        <span role="img">🍿</span>
+        <h1 className="text-white font-bold text-xl">usePopcorn</h1>
+      </div>
+      <input
+        className="search px-4 py-2 rounded-md border border-gray-300 focus:outline-none focus:ring-2 focus:ring-blue-500 text-white"
+        type="text"
+        placeholder="Search movies..."
+        value={query}
+        onChange={(e) => setQuery(e.target.value)}
+      />
+      <p className="num-results text-end text-white text-lg">
+        Found <strong>{movies.length}</strong> results
+      </p>
+    </nav>
+  );
+};
+
+export default Navbar;
--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,12 @@ import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
 export default defineConfig({
+  theme: {
+    extend: {
+      colors: {
+        primary:   // Replace with your actual color code
+      },
+    },
+  },
   plugins: [react(), tailwindcss(),],
 })
