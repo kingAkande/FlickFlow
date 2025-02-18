9f4d902f8772881d54c29bee3500d1f38ad9e70d
KingAkande <kingakande@gmail.com>
2025-02-18 22:30:52 +0100

    reusability update to rating's component

--- src/Components/StarRating.jsx
+++ src/Components/StarRating.jsx
@@ -3,8 +3,9 @@
 
 import React, { useState } from "react";
 
-const StarRating = ({ max = 2 }) => {
+const StarRating = ({ max = 2, color, className }) => {
   const [rating, setRating] = useState(0);
+  const [tempoRating, setTempoRating] = useState(0);
   function handleRate(rate) {
     setRating(rate);
   }
@@ -12,25 +13,46 @@ const StarRating = ({ max = 2 }) => {
     <div className="flex gap-8 items-center ">
       <div className="flex gap-2">
         {Array.from({ length: max }, (_, i) => (
-          <Star key={i} handleRate={() => handleRate(i+1)} full={rating >= i+1} />
+          <Star
+            key={i}
+            handleRate={() => handleRate(i + 1)}
+            full={tempoRating ? tempoRating >= i + 1 : rating >= i + 1}
+            handleMouseEnter={() => setTempoRating(i + 1)}
+            handleMouseLeave={() => setTempoRating(0)}
+            color={color}
+            className={className}
+          />
         ))}
       </div>
-      <p className="leading-5 m-0">{rating || ""}</p>
+      <p className="leading-5 text-amber-300 m-0">{tempoRating || rating || ""}</p>
     </div>
   );
 };
 
 export default StarRating;
 
-function Star({ handleRate, index, full }) {
+function Star({
+  color,
+  className = "h-8 w-8 cursor-pointer ",
+  handleRate,
+  full,
+  handleMouseEnter,
+  handleMouseLeave,
+}) {
   return (
-    <span onClick={handleRate} className="h-8 w-8">
-      { full   ? (
+    <span
+      onClick={handleRate}
+      onMouseEnter={handleMouseEnter}
+      onMouseLeave={handleMouseLeave}
+      color={color}
+      className={className}
+    >
+      {full ? (
         <svg
           xmlns="http://www.w3.org/2000/svg"
           viewBox="0 0 20 20"
-          fill="#000"
-          stroke="#000"
+          fill={color}
+          stroke={color}
         >
           <path d="M9.049 2.927c.3-.921 1.603-.921 1.902 0l1.07 3.292a1 1 0 00.95.69h3.462c.969 0 1.371 1.24.588 1.81l-2.8 2.034a1 1 0 00-.364 1.118l1.07 3.292c.3.921-.755 1.688-1.54 1.118l-2.8-2.034a1 1 0 00-1.175 0l-2.8 2.034c-.784.57-1.838-.197-1.539-1.118l1.07-3.292a1 1 0 00-.364-1.118L2.98 8.72c-.783-.57-.38-1.81.588-1.81h3.461a1 1 0 00.951-.69l1.07-3.292z" />
         </svg>
--- src/main.jsx
+++ src/main.jsx
@@ -1,14 +1,14 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import StarRating from './Components/StarRating'
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+import "./index.css";
+import StarRating from "./Components/StarRating";
 //import App from './App.jsx'
 
-createRoot(document.getElementById('root')).render(
+createRoot(document.getElementById("root")).render(
   <StrictMode>
     {/* <App /> */}
-    <StarRating max={5}/>
-    <StarRating max={10}/>   
-
-  </StrictMode>,
-)
+    <StarRating color="red" className="h-6 w-6 text-red-600 " max={5} />
+    <StarRating max={10} />
+  </StrictMode>
+);
+ 
